b0bfa7c9c3b9be305644183412f13351bc638277
arkajitroy <arkajitroy11.official@gmail.com>
2023-12-10 21:14:41 +0530

    added: P1_clone_graph Graph

---
+++ Graph_I/P1_Clone_graph.java
@@ -0,0 +1,70 @@
+package Graph_I;
+import java.util.*;
+
+class Node{
+    public int val;
+    public List<Node> neighbors;
+
+    public Node(){
+        val = 0;
+        neighbors = new ArrayList<>();
+    }
+
+    public Node(int val){
+        this.val = val;
+        neighbors = new ArrayList<>();
+    }
+
+    public Node(int val, List<Node> neighbors){
+        this.val = val;
+        this.neighbors = neighbors;
+    }
+}
+
+class Main {
+
+    private Map<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        Node cloneNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, cloneNode);
+
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return cloneNode;
+    }
+    
+    public static void main(String[] args) {
+        Main cloneGraph = new Main();
+
+        // Create a sample graph
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+
+        node1.neighbors.add(node2);
+        node1.neighbors.add(node4);
+
+        node2.neighbors.add(node1);
+        node2.neighbors.add(node3);
+
+        node3.neighbors.add(node2);
+        node3.neighbors.add(node4);
+
+        node4.neighbors.add(node1);
+        node4.neighbors.add(node3);
+
+        Node clonedNode = cloneGraph.cloneGraph(node1);
+    }
+}
